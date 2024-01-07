d1b4800b5b644da8b8d3e75f74aa35201514d707
kali.eg <kali.eg@solutions.com.sa>
2024-01-08 00:31:39 +0200

    add database settings for application

--- Academy_crm/settings.py
+++ Academy_crm/settings.py
@@ -98,11 +98,24 @@ AUTH_USER_MODEL = "clients.User"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+        "ENGINE": env(
+            "SQL_ENGINE"
+        ),
+        "NAME": env("SQL_DATABASE"),
+        "USER": env("SQL_USER"),
+        "PASSWORD": env("SQL_PASSWORD"),
+        "HOST": env("SQL_HOST"),
+        "PORT": env("SQL_PORT"),
+        "ATOMIC_REQUESTS": True,
+    },
 }
 
 
