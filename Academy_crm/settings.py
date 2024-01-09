fd845e999c829fb8661e05692f060a16cd28ce69
kali.eg <kali.eg@solutions.com.sa>
2024-01-09 12:51:50 +0200

    push settings for the server

--- Academy_crm/settings.py
+++ Academy_crm/settings.py
@@ -99,25 +99,25 @@ AUTH_USER_MODEL = "clients.User"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
 # DATABASES = {
 #     "default": {
-#         "ENGINE": env(
-#             "SQL_ENGINE"
-#         ),
-#         "NAME": env("SQL_DATABASE"),
-#         "USER": env("SQL_USER"),
-#         "PASSWORD": env("SQL_PASSWORD"),
-#         "HOST": env("SQL_HOST"),
-#         "PORT": env("SQL_PORT"),
-#         "ATOMIC_REQUESTS": True,
-#     },
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
 # }
+DATABASES = {
+    "default": {
+        "ENGINE": env(
+            "SQL_ENGINE"
+        ),
+        "NAME": env("SQL_DATABASE"),
+        "USER": env("SQL_USER"),
+        "PASSWORD": env("SQL_PASSWORD"),
+        "HOST": env("SQL_HOST"),
+        "PORT": env("SQL_PORT"),
+        "ATOMIC_REQUESTS": True,
+    },
+}
 
 
 # Password validation
